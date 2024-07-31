2a26e70f210595f3adead26c5334c2f73a064d2f
Rizwansiddi <rizwan.sndigitech@gmail.com>
2024-07-31 16:54:18 +0530

    Update config.js

--- _configs/config.js
+++ _configs/config.js
@@ -1,5 +1,5 @@
 export const config = {
-  url: 'mongodb+srv://rizwan:rizwan@cluster0.vxqcp.mongodb.net/weather?retryWrites=true&w=majority',
-  apiKey: '0021b3866a9b989d5512a75622796a0d',
-  baseURL: "https://api.openweathermap.org/data/2.5/",
+  url: '',
+  apiKey: '',
+  baseURL: "",
 };
